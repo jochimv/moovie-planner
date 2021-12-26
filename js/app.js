54f81c0ab42c234653fc51804cedc7c6d4249a12
Václav Jochim <VJochim@cpas.cz>
2021-12-26 22:31:00 +0100

    added alerts

--- js/app.js
+++ js/app.js
@@ -1,234 +1,258 @@
 const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-    const main = $('#main');
-    const form = $('#form');
-    const search = $('#search');
-    const button = $('#search-button');
+const main = $('#main');
+const form = $('#form');
+const search = $('#search');
+const button = $('#search-button');
 
-    showMovies(apiUrl);
+showMovies(apiUrl);
 
-    function showMovies(url){
-      main.empty();
+function showMovies(url) {
+  main.empty();
 
-        fetch(url).then(res => res.json())
-        .then((data) => {
+  fetch(url).then(res => res.json())
+    .then((data) => {
+      if (data.total_results == 0) {
 
-            data.results.forEach(element => {
-            
-              const container = $('<div></div>').addClass('container');
-              const column1 = $('<div></div>').addClass('column');
-              const column2 = $('<div></div>').addClass('column');
-    
-            const image = document.createElement('img');
-            image.src = IMGPATH + element.poster_path;
-            column2.append(image);
-    
-            const name = $('<h2></h2>').text(element.title);
-    
-            const rating = $('<div></div>').addClass('rating').html(
-                convertToStars(element.vote_average)
-            );
-            
-            const releaseYear = $('<div></div>').html(`<strong>Release: </strong>${getReleaseYear(element.release_date)}`);
-            const description = $('<div></div>').text(element.overview);
-    
-            const pickerContainer = $('<div></div>').addClass('picker-container');
-    
-            const dateRow = $('<div></div>').addClass('row');
-            const dateText = $('<p></p>').html('<strong>Select a date:</strong>');
-    
-            const dateInput = $('<input/>').attr('type','date');
-            
-            dateRow.append(dateText,dateInput);
-    
-            const timeRow = $('<div></div>').addClass('row');
-            const timeText = $('<p></p>').html('<strong>Select a time:</strong>');
-            const timeInput = $('<input/>').attr('type','time');
+        const emptyText = $('<h2>No movies found.</h2>').addClass('no-movies');
+        main.append(emptyText);
 
-            timeRow.append(timeText,timeInput);
+      } else {
+        data.results.forEach(element => {
 
-            getDurationById(element.id).then((length) => {
-              const duration =  $('<div></div>').html(`<strong>Length: </strong> ${length} min`);
-              const submitButton = $('<button></button>').addClass('submit-button').text('Add to google calendar').click(() => {
-              let startDateString = calculateEndDateString(dateInput.val(),timeInput.val(),0);
-              let endDateString = calculateEndDateString(dateInput.val(),timeInput.val(),length);
-                console.log(startDateString);
-                console.log(endDateString);
+          const container = $('<div></div>').addClass('container');
+          const column1 = $('<div></div>').addClass('column');
+          const column2 = $('<div></div>').addClass('column');
 
+          let image = undefined;
+
+          if (element.poster_path == null) {
+            image = $('<div></div>').text('Poster for this moovie is missing').addClass('missing');
+          } else {
+            image = $('<img/>').attr('src', IMGPATH + element.poster_path).attr('alt', `Poster of ${element.title}`);
+          }
+          column2.append(image);
+
+          const name = $('<h2></h2>').text(element.title);
+
+          const rating = $('<div></div>').addClass('rating').html(
+            convertToStars(element.vote_average)
+          );
+
+          const releaseYear = $('<div></div>').html(`<strong>Release: </strong>${getReleaseYear(element.release_date)}`).addClass('release-year');
+          const description = $('<div></div>').text(element.overview);
+
+          const pickerContainer = $('<div></div>').addClass('picker-container');
+
+          const dateRow = $('<div></div>').addClass('row');
+          const dateText = $('<p></p>').html('<strong>Select a date:</strong>');
+
+          const dateInput = $('<input/>').attr('type', 'date');
+
+          dateRow.append(dateText, dateInput);
+
+          const timeRow = $('<div></div>').addClass('row');
+          const timeText = $('<p></p>').html('<strong>Select a time:</strong>');
+          const timeInput = $('<input/>').attr('type', 'time');
+
+          timeRow.append(timeText, timeInput);
+
+          getDurationById(element.id).then((length) => {
+            let lenghtText = length == 0 ? 'unknown' : `${length} min`;
+            const duration = $('<div></div>').html(`<strong>Length: </strong> ${lenghtText}`).addClass('length');
+            const submitButton = $('<button></button>').addClass('submit-button').text('Add to google calendar').click(() => {
+
+              if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+
+                if (dateInput.val() == '' && timeInput.val() == '') {
+                  alert('Enter date and time first.');
+                } else if (dateInput.val() == '') {
+                  alert('Enter date first.');
+                } else if (timeInput.val() == '') {
+                  alert('Enter time first.');
+                };
+                let startDateString = calculateEndDateString(dateInput.val(), timeInput.val(), 0);
+                let endDateString = calculateEndDateString(dateInput.val(), timeInput.val(), length);
+
+                console.log(gapi.auth2.getAuthInstance().isSignedIn.get());
                 var event = {
                   'summary': `Watch ${element.title}`,
                   'start': {
-                  'dateTime': `${startDateString}-00:00`,
-                  'timeZone': 'Europe/Prague'
+                    'dateTime': `${startDateString}-00:00`,
+                    'timeZone': 'Europe/Prague'
                   },
                   'end': {
-                  'dateTime': `${endDateString}-00:00`,
-                  'timeZone': 'Europe/Prague'
+                    'dateTime': `${endDateString}-00:00`,
+                    'timeZone': 'Europe/Prague'
                   },
                   'reminders': {
-                  'useDefault': false,
-                  'overrides': [
-                    {'method': 'email', 'minutes': 24 * 60},
-                    {'method': 'popup', 'minutes': 10}
-                  ]
+                    'useDefault': false,
+                    'overrides': [
+                      { 'method': 'email', 'minutes': 24 * 60 },
+                      { 'method': 'popup', 'minutes': 10 }
+                    ]
                   }
-                  };
-              
+                };
+
                 var request = gapi.client.calendar.events.insert({
                   'calendarId': 'primary',
                   'resource': event
-                  });
+                });
 
-                  
-                request.execute(function(event) {
+
+                request.execute(function (event) {
                   alert(`${element.title} was added to your google calendar!
                   \nDate: ${convertToEuropeDate(dateInput.val())}
                   \nTime: ${timeInput.val()}`);
-              });
-         
+                });
+
+              } else {
+                alert('Log in to your google account first.')
+              }
             });
-              column1.append(name,rating,duration,releaseYear,description,pickerContainer);
-              pickerContainer.append(dateRow,timeRow,submitButton);
-              container.append(column1,column2);
-              main.append(container);
-            }); 
-        }); 
+            column1.append(name, rating, duration, releaseYear, description, pickerContainer);
+            pickerContainer.append(dateRow, timeRow, submitButton);
+            container.append(column1, column2);
+            main.append(container);
+          });
+        });
+      };
     });
-    };
+};
 
-    const convertToEuropeDate = (dateInput) => {
-      const dateValues = dateInput.split('-');
-      let humanReadableDate = [];
-      for(let i = dateValues.length - 1; i >= 0; i--){
-        humanReadableDate.push(dateValues[i]);
-      }
-      return humanReadableDate.join('/');
-    };
+const convertToEuropeDate = (dateInput) => {
+  const dateValues = dateInput.split('-');
+  let humanReadableDate = [];
+  for (let i = dateValues.length - 1; i >= 0; i--) {
+    humanReadableDate.push(dateValues[i]);
+  }
+  return humanReadableDate.join('/');
+};
 
-    const calculateEndDateString = (dateString,timeString,minutes) => {
-      const date = new Date(dateString + 'T' + timeString);
-      return addMinutes(date,minutes);
-    }
+const calculateEndDateString = (dateString, timeString, minutes) => {
+  const date = new Date(dateString + 'T' + timeString);
+  return addMinutes(date, minutes);
+}
 
-    const addMinutes = (date, minutes) => {
-      return transferDateToGoogleApiString(new Date(date.getTime() + minutes * 60000));
-    }
+const addMinutes = (date, minutes) => {
+  return transferDateToGoogleApiString(new Date(date.getTime() + minutes * 60000));
+}
 
 
-    const transferDateToGoogleApiString = (date) => {
-      const isoString = date.toISOString();
-      const isoArray = isoString.split('.');
-      console.log(isoArray[0]);
-      return isoArray[0];
-    }
+const transferDateToGoogleApiString = (date) => {
+  const isoString = date.toISOString();
+  const isoArray = isoString.split('.');
+  console.log(isoArray[0]);
+  return isoArray[0];
+}
 
-    const getReleaseYear = (releaseDate) => {
-      const fullDate = releaseDate.split('-');
-      return fullDate[0];
-    }
+const getReleaseYear = (releaseDate) => {
+  console.log(releaseDate);
+  const fullDate = releaseDate.split('-');
+  return fullDate[0] != '' ? fullDate[0] : 'unknown';
+}
 
-    const convertToStars = (rating) => {
-        const numberOfStars = Math.round(rating / 2);
-        let stars = '';
-        for(let i = 0; i < numberOfStars; i++){
-            stars = stars + '&starf;';
-        }
-        return stars;
-    }
-    
-    button.click((e) => {
-        e.preventDefault();
-         
-        showMovies(SEARCHAPI + search.val());
+const convertToStars = (rating) => {
+  const numberOfStars = Math.round(rating / 2);
+  let stars = '';
+  for (let i = 0; i < numberOfStars; i++) {
+    stars = stars + '&starf;';
+  }
+  return stars;
+}
+
+button.click((e) => {
+  e.preventDefault();
+
+  showMovies(SEARCHAPI + search.val());
+});
+
+form.submit((e) => {
+  e.preventDefault();
+  showMovies(SEARCHAPI + search.val());
+});
+
+const getDurationById = (movieId) => {
+  return fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=04c35731a5ee918f014970082a0088b1`).then(res => res.json())
+    .then(data => {
+      duration = data.runtime;
+      return duration;
     });
+};
 
-    form.submit((e) => {
-      e.preventDefault();
-      showMovies(SEARCHAPI + search.val());
-    });
 
-    const getDurationById = (movieId) => {
-      return fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=04c35731a5ee918f014970082a0088b1`).then(res => res.json())
-      .then(data => {
-        duration = data.runtime;
-          return duration;
-      });
-    
-    };
-   
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '779681715031-0u954k7mb1545tuhl6e1k2d6u62caokr.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDdopeSLDxcJlI66BplqPTH6TqJnMb05LI';
 
-  // Client ID and API key from the Developer Console
-  var CLIENT_ID = '779681715031-0u954k7mb1545tuhl6e1k2d6u62caokr.apps.googleusercontent.com';
-  var API_KEY = 'AIzaSyDdopeSLDxcJlI66BplqPTH6TqJnMb05LI';
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
-  // Array of API discovery doc URLs for APIs used by the quickstart
-  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar";
 
-  // Authorization scopes required by the API; multiple scopes can be
-  // included, separated by spaces.
-  var SCOPES = "https://www.googleapis.com/auth/calendar";
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
 
-  var authorizeButton = document.getElementById('authorize_button');
-  var signoutButton = document.getElementById('signout_button');
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
 
-  /**
-   *  On load, called to load the auth2 library and API client library.
-   */
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function (error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
   }
+}
 
-  /**
-   *  Initializes the API client library and sets up sign-in state
-   *  listeners.
-   */
-  function initClient() {
-    gapi.client.init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES
-    }).then(function () {
-      // Listen for sign-in state changes.
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
 
-      // Handle the initial sign-in state.
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      authorizeButton.onclick = handleAuthClick;
-      signoutButton.onclick = handleSignoutClick;
-    }, function(error) {
-      appendPre(JSON.stringify(error, null, 2));
-    });
-  }
-
-  /**
-   *  Called when the signed in status changes, to update the UI
-   *  appropriately. After a sign-in, the API is called.
-   */
-  function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-      authorizeButton.style.display = 'none';
-      signoutButton.style.display = 'block';
-    } else {
-      authorizeButton.style.display = 'block';
-      signoutButton.style.display = 'none';
-    }
-  }
-
-  /**
-   *  Sign in the user upon button click.
-   */
-  function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-  }
-
-  /**
-   *  Sign out the user upon button click.
-   */
-  function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-  }
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
 
